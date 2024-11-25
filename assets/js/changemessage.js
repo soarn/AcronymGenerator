1df9ed088f535ab776924331d309c7bb4648fde4
Jack <soarn@soarn.pro>
2024-11-25 13:21:42 -0700

    feat: implement real acronyms for the time when you really need to know what it stands for

--- assets/js/changemessage.js
+++ assets/js/changemessage.js
@@ -1,6 +1,8 @@
 let words;
+let real;
 
 fetch('./assets/json/words.json').then(r => r.json()).then(data => words = data);
+fetch('./assets/json/real.json').then(r => r.json()).then(data => real = data);
 
 /**
  * Changes the message displayed on the main page
@@ -18,13 +20,19 @@ function changeMessage(acronym, phrase) {
 function generateAcronym() {
   let acronym = document.getElementById("acronym").value;
   acronym = acronym.toLowerCase();
-  const letters = acronym.split("");
-  let phrase = "";
-  for (let i = 0; i < letters.length; i++) {
-    phrase += generateWord(letters[i]);
-    phrase += " ";
+  if (real.hasOwnProperty(acronym)) {
+    phrase = retrievePhrase(acronym);
+    changeMessage(acronym, phrase);
+  } 
+  else {
+    const letters = acronym.split("");
+    let phrase = "";
+    for (let i = 0; i < letters.length; i++) {
+      phrase += generateWord(letters[i]);
+      phrase += " ";
+    }
+    changeMessage(acronym, phrase);
   }
-  changeMessage(acronym, phrase);
 }
 
 /**
@@ -36,6 +44,22 @@ function generateWord(letter) {
   let array = [];
   if (letter.toString().toLowerCase().match(/[a-z]/))
     array = words[letter].split(',');
+
+  if (array.length > 0)
+    return array[Math.floor(Math.random() * array.length)];
+  else
+    return "";
+}
+
+/**
+ * Retrieves a phrase for a given acronym, or "" for invalid character
+ * @param phrase {string} The phrase
+ * @returns {string} A defined phrase for the specified acronym
+ */
+function retrievePhrase(phrase) {
+  let array = [];
+  if (phrase.toString().toLowerCase())
+    array = real[phrase].split(',');
 
   if (array.length > 0)
     return array[Math.floor(Math.random() * array.length)];
